2ea2eba6db8321097e101ef53908b056f515cf71
Kevin Chhan <kvnchhan@gmail.com>
2020-10-12 12:02:33 -0700

    Updates

---
+++ precache-manifest.c5fc68155456a9f2df61db2c3ecccbbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec22d62e34717440a5a47801cfed1782",
+    "url": "/kchhan/memory-game/index.html"
+  },
+  {
+    "revision": "f78d8388c11b68a34b93",
+    "url": "/kchhan/memory-game/static/css/main.7ca88f4e.chunk.css"
+  },
+  {
+    "revision": "09c1bfbfb6fe4fa64c5b",
+    "url": "/kchhan/memory-game/static/js/2.4121aeb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kchhan/memory-game/static/js/2.4121aeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78d8388c11b68a34b93",
+    "url": "/kchhan/memory-game/static/js/main.4d4c96b5.chunk.js"
+  },
+  {
+    "revision": "1c73c89a4d7acf5ed9bc",
+    "url": "/kchhan/memory-game/static/js/runtime-main.c942e0e8.js"
+  }
+]);
